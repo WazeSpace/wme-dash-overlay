f6265ba41af1ff880c5f5566d8833545d9869485
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2024-12-31 11:49:41 +0200

    chore: update package.json and tampermonkey.meta.js config for the script

--- tampermonkey.meta.js
+++ tampermonkey.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name WME Userscript Template
-// @description A basic template for userscripts
+// @name WME Dash Overlay
+// @description See your dashcam videos right on the map in WME and make editing a breeze
 // @version 1.0.0
-// @author Your name here
+// @author r0den
 // @match https://*.waze.com/*editor*
 // @match https://waze.com/*editor*
 // @match https://*.wazestg.com/*editor*
